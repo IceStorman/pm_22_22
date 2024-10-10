d34f66565e55350527ce4d1c2fb2ebe01120fc45
IceStorman <vladgamedeveloper@gmail.com>
2024-10-10 11:04:18 +0300

    Added animation for progress bars

--- app/js/script.js
+++ app/js/script.js
@@ -10,15 +10,40 @@ document.addEventListener("DOMContentLoaded", function()
 
     function toggleSection(section)
     {
+        const tracks = section.getElementsByClassName('track');
+        const thumbs = section.getElementsByClassName('thumb');
+
         if (section.style.maxHeight)
         {
-            console.log("off");
             section.style.maxHeight = null;
+
+            if(tracks.length > 0)
+            {
+                console.log('off');
+                Array.from(tracks).forEach(track => {
+                    track.style.width = 0 + '%';
+                })
+                Array.from(thumbs).forEach(thumb => {
+                    thumb.style.left = 0 + '%';
+                })
+            }
         }
         else
         {
-            console.log("on");
             section.style.maxHeight = "300px";
+
+            if(tracks.length > 0)
+            {
+                console.log('on');
+                Array.from(tracks).forEach(track => {
+                    const value = track.getAttribute('data-value');
+                    track.style.width = value + '%';
+                })
+                Array.from(thumbs).forEach(thumb => {
+                    const value = thumb.getAttribute('data-value');
+                    thumb.style.left = value + '%';
+                })
+            }
         }
     }
 });
